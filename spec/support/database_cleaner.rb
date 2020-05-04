ff355fb13bfc6a86ad8289e3e41a50641ba9c2dd
Rodrigo Penloski <r.penloski@gmail.com>
2020-05-04 12:37:20 -0300

    Add missing gem and enable the spec/support directory

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -4,7 +4,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     FactoryBot.lint
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, except: %w[ar_internal_metadata])
   end
 
   config.around(:each) do |example|
