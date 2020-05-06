a5721f9f06cd2c69db37df91f52f0a80405ea428
Rodrigo Penloski <r.penloski@gmail.com>
2020-05-06 00:28:24 -0300

    Add transaction methods to the account model  - Add deposit  - Add withdraw  - Add total_balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,4 +5,18 @@ class Account < ApplicationRecord
   validates :id, uniqueness: true
 
   has_many :transactions
+
+  def total_balance
+    transactions.reduce(balance) do |sum, transaction|
+      sum + (transaction.deposit? ? transaction.amount : -transaction.amount)
+    end
+  end
+
+  def deposit(amount)
+    transactions.deposit.create(amount: amount)
+  end
+
+  def withdraw(amount)
+    transactions.withdraw.create(amount: amount)
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe Account, type: :model do
   describe 'associations' do
     it { should have_many :transactions }
   end
+
+  describe '#total_balance' do
+    let(:account) { FactoryBot.create(:account, balance: 1000.0) }
+
+    it 'returns the total balance for the account' do
+      account.deposit(100.00)
+      account.withdraw(200.50)
+      expect(account.total_balance).to eq 899.50
+    end
+  end
+
+  describe '#deposit' do
+    let(:account) { FactoryBot.create(:account, balance: 1000.0) }
+
+    before { account.deposit(250.0) }
+    it 'creates a new deposit transaction for the account' do
+      expect(account.transactions.deposit.last.amount).to eq 250.0
+      expect(account.transactions.last).to be_deposit
+    end
+  end
+
+  describe '#withdraw' do
+    let(:account) { FactoryBot.create(:account, balance: 1000.0) }
+
+    before { account.withdraw(250.0) }
+    it 'creates a new withdraw transaction for the account' do
+      expect(account.transactions.withdraw.last.amount).to eq 250.0
+      expect(account.transactions.last).to be_withdraw
+    end
+  end
 end
